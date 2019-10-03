ba66fcee67e77ca1ecdd7fd1c7e4fa8cae4842fb
VinoTan88 <vino8959@gmail.com>
2019-10-03 12:49:44 +0700

    update

---
+++ storyApps/src/app/list/list.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: 'list.page.html',
+  styleUrls: ['list.page.scss']
+})
+export class ListPage implements OnInit {
+  private selectedItem: any;
+  private icons = [
+    'paper-plane'
+  ];
+  public items: Array<{ title: string; note: string; icon: string }> = [];
+  constructor() {
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Video ' + i,
+        note: 'Story 2019',
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
+  }
+
+  ngOnInit() {
+  }
+  // add back when alpha.4 is out
+  // navigate(item) {
+  //   this.router.navigate(['/list', JSON.stringify(item)]);
+  // }
+}
